e19d7dece4849a8e2d2157f0ebedcafc41b2c00e
Darren Kim <dkim555@yahoo.com>
2023-05-03 14:51:56 -0700

    add propTypes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import Button from "./Button";
 
 function App() {
     return (
-        <div>App</div>
+        <div>
+            <div>
+                <Button danger outline>Click Me</Button>
+            </div>
+            <div>
+                <Button success rounded>Dont Click me</Button>
+            </div>
+            <div>
+                <Button warning outline>huy</Button>
+            </div>
+            <div>
+                <Button primary rounded>Bro</Button>
+            </div>
+            <div>
+                <Button secondary>Dang</Button>
+            </div>
+        </div>
     )
     
 }
