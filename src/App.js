c0ac0c18338af025360a3c2911b5df7cad2ed7b4
Darren Kim <dkim555@yahoo.com>
2023-05-09 13:36:55 -0700

    add map function

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Accordion from "./components/Accordion";
 function App() {
     const items = [
         {
+            id: 'ert',
             label : 'Can I use React?',
             content : 'You can you anything'
         },
         {
+            id: "sdfgsd",
             label : 'Can I use Javascript?',
             content : 'You can you anything'
         },
         {
+            id: 'wefre',
             label : 'Can I use CSS?',
             content : 'You can you anything'
         },
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,8 +1,25 @@
-
+import { useState } from "react";
 
 function Accordion ({items}) {
+    const [expandedIndex, setExpandedIndex] = useState(0);
+
+    const renderedItems = items.map((item, index) => {
+       const isExpanded = index === expandedIndex;
+
+       const content = isExpanded && <div>{item.content}</div>
+
+        return (
+            <div key={item.id}>
+                <div>{item.label}</div>
+                {content}
+            </div>
+        );
+    });
+
     return(
-        <div></div>
+        <div>
+            {renderedItems}
+        </div>
     )
 }
 
